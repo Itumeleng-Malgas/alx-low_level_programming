45bbe447123c0ce2554f23c9b1e62d2eadd82583
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-17 20:20:13 +0200

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 113 || i == 101)
+			continue;
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
