587e0ab6339cde0eb911d380fa629ac930b3d67b
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-20 16:17:54 +0200

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,17 +10,17 @@ int main(void)
 
 	for (i = 0; i < 1000; i++)
 	{
-		int unit = i      % 10;
-		int tens = i / 10 % 10;
+		int unit = i       % 10;
+		int tens = i / 10  % 10;
 		int hund = i / 100 % 10;
 
-		if (unit < tens && tens < hund)
+		if (unit > tens && tens > hund)
 		{
-			putchar(unit + '0');
-			putchar(tens + '0');
 			putchar(hund + '0');
+			putchar(tens + '0');
+			putchar(unit + '0');
 
-			if (hund == 9 && tens == 8 && unit == 7)
+			if (hund == 7 && tens == 8 && unit == 9)
 				break;
 			putchar(',');
 			putchar(' ');
