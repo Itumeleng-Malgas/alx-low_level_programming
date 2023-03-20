8e524b29d78e515d618a19e4a2905048a970fc1f
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-20 19:36:52 +0200

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "_putchar.h"
+#include <stdio.h>
+
+/**
+ * _putchar - Prints "_putchar"
+ */
+void _putchar(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+}
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar();
+	return (0);
+}
