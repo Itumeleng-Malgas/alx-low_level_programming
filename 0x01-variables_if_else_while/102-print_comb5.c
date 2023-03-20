3b6470964165b56362a140f9ca83fc30e55b4532
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-20 17:01:16 +0200

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i, n;
+
+	for (i = 0; i <= 98; i++)
+	{
+		int unit = i       % 10;
+		int tens = i / 10  % 10;
+
+		for (n = i;; n++)
+		{
+			int unit_n = n       % 10;
+			int tens_n = n / 10  % 10;
+
+			putchar(tens + '0');
+			putchar(unit + '0');
+			putchar(' ');
+			putchar(tens_n + '0');
+			putchar(unit_n + '0');
+			if (i == 98 && n == 99)
+				break;
+
+			putchar(',');
+			putchar(' ');
+
+			if (n == 99)
+				break;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
