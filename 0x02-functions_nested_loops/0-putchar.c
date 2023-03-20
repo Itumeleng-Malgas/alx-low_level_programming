e6907999cca08781778be401c0425ea7d4bd83c5
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-20 19:53:28 +0200

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,4 @@
-#include "_putchar.h"
-#include <stdio.h>
-
-/**
- * _putchar - Prints "_putchar"
- */
-void _putchar(void)
-{
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-}
+#include "main.h"
 
 /**
  * main - Entry point
