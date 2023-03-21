a64bb9889452f7881238bc66c43d143875ef57d9
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-21 06:50:38 +0200

    1-alphabet.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,9 +1,5 @@
 #include <unistd.h>
 
-/**
- * _putchar - prints a single character to file descriptor 1 (stdout)
- * @c: characters to be printed
- */
 void _putchar(char c)
 {
 	write(1, &c, sizeof(c));
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - Prints a single character to file descriptor 1 (stdout)
+ * @c: Character to be printed
+ */
 void _putchar(char c);
+
+/**
+ * print_alphabet - Prints the alphabets, in lowercase
+ */
+void print_alphabet(void);
 
 #endif /* MAIN_H */
