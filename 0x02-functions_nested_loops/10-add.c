086873d4199d78e9f307e50275362ca1b9b483e9
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-22 00:58:36 +0200

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,30 +1,6 @@
 #include "main.h"
 
 /**
- * power - computer the exponent
- * Return: the exponent
- * @base: the base
- * @exponent: the exponent
- */
-int power(int base, int exponent)
-{
-	if (exponent == 0)
-	{
-		return (1);
-	}
-	else if (exponent % 2 == 0)
-	{
-		int result = power(base, exponent / 2);
-
-		return (result * result);
-	}
-	else
-	{
-		return (base * power(base, exponent - 1));
-	}
-}
-
-/**
  * add - adds two numbers
  * Return: The sum
  * @a: first value
@@ -32,11 +8,5 @@ int power(int base, int exponent)
  */
 int add(int a, int b)
 {
-	int result, num_digits, temp, i;
-
-	result = a + b;
-	num_digits = 0;
-	temp = result;
-
-	return (result);
+	return (a + b);
 }
