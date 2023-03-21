67fab68859f3561a0198150cbd142a18381c4edf
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-21 10:13:48 +0200

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of any number
+ * @i: integer value to be searched
+ * Return: The value of that last digit
+ */
+int print_last_digit(int i)
+{
+	return ((i % 10));
+}
