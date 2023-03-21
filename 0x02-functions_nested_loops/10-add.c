ff24ba9070ea065f34a613578b7080e2a48d15d5
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-22 00:54:17 +0200

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -38,18 +38,5 @@ int add(int a, int b)
 	num_digits = 0;
 	temp = result;
 
-	/* Count the number of digits in the result */
-	while (temp != 0)
-	{
-		temp /= 10;
-		num_digits++;
-	}
-
-	for (i = (num_digits - 1); i >= 0; i--)
-	{
-		_putchar((result / power(10, i)) % 10 + '0');
-	}
-	_putchar('\n');
-
 	return (result);
 }
