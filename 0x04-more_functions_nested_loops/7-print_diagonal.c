f8a3ca5c946919ace4353dd142b9a5a8e142d590
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-23 19:38:24 +0200

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,20 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (n <= 0)
 			break;
 
-		for (j = 0; j <= i; j++)
+		for (j = 0; j < i; j++)
+		{
 			_putchar(' ');
+		}
 		_putchar('\\');
 		_putchar('\n');
+
+		if (i == n)
+			return;
 	}
-	_putchar('\n');
 }
 
--- 0x04-more_functions_nested_loops/run.c
+++ 0x04-more_functions_nested_loops/run.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
-	print_diagonal(10);
+	print_diagonal(0);
+	
 	return (0);
 }
