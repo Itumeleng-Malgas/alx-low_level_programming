35dfa8d34a9f028717191f3320156cf1d4280638
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-23 14:51:29 +0200

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints numbers 0-9 and !2 & !4
+*/
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		if ((num != 2) || (num !=4))
+			_putchar(num + '0');
+	}
+}
