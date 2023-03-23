e893673c6c2dbb3b6e24915be42dea94247d67fe
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-23 22:07:24 +0200

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+
 /**
-* main - fizzbuzz game
+* main - fizz buzz game
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
@@ -11,13 +11,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+
+		if (i != 100)
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
