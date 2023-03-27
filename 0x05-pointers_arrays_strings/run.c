1f79d79b4147ef73cdac8389f7487aae5e4a571c
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-27 18:22:21 +0200

    9-strcpy.c

--- 0x05-pointers_arrays_strings/run.c
+++ 0x05-pointers_arrays_strings/run.c
@@ -3,8 +3,13 @@
 
 int main(void)
 {
-	int arr[] = {1,2,3};
+	char s1[98];
+	char *ptr;
 
-	print_array(arr, 3);
+	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+
+	printf("%s", s1);
+	printf("%s", ptr);
+
 	return (0);
 }
