dfda4cb9fbfd3a6c923acc55f0da0459514e5261
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-27 18:36:24 +0200

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,15 +9,10 @@ void puts_half(char *str)
 {
 	unsigned long second_halve, length_of_the_string = strlen(str);
 
-	if (length_of_the_string % 2 == 0)
-		second_halve = length_of_the_string / 2;
-	else
-		second_halve = (length_of_the_string - 1) / 2;
-
+	second_halve = length_of_the_string / 2;
 	while (second_halve < length_of_the_string)
 	{
 		_putchar(str[second_halve]);
 		second_halve++;
 	}
-	_putchar('\n');
 }
