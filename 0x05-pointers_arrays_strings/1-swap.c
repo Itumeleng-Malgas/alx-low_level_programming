ab679cb88c0fdd5c2512f0b3f9069338f6c70d9a
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-27 09:56:05 +0200

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/**
+* swap_int - swap values of two integers
+* @a: first value
+* @b: second value
+*/
+
+void swap_int(int *a, int *b)
+{
+	int *temp = &a;
+
+	a = &b;
+	b = &temp;
+}
+
+
