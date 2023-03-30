920052903b01c2dabc2fdd02c8191d64826d0069
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-03-30 10:22:36 +0200

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,11 +14,17 @@ char *_strncpy(char *dest, char *src, int n)
 	char *ptr = dest;
 	int i = 0;
 
-	while (++i < n && *src != '\0')
+	while (i < n && *src != '\0')
+	{
 		*ptr++ = *src++;
+		i++;
+	}
 
 	while (++i < n)
+	{
 		*ptr++ = '\0';
+		i++;
+	}
 
 	return (dest);
 }
