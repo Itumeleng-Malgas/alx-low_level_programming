e98b26e91115963bbdfab705b45db6a1fa4e23fd
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-02 05:20:04 +0200

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strspn - gets the length of a prefix substring
+* @s: initial string
+* @accept: s substring
+* Return: number of bytes from accept
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+
+	while (*s++)
+	{
+		if (*s == *accept++)
+			n += 1;
+	}
+}
