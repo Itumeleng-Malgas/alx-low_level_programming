525f929cb79dcbf6d5b48f64b7513de05e910fc5
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-06 03:01:23 +0200

    0-puts_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,56 +1,17 @@
 #include "main.h"
 
-/**
-* square_root - computes natural square root of a number
-* @n: integer to be squared
-* @start: int
-* @end: int
-* Return: n squared
-*/
-
-int square_root(int n, int start, int end)
+int _sqrt_recursion(int n)
 {
-	int mid = (start + end) / 2;
-
-	if (start > end)
-	{
-		return (end);
-	}
-	if (mid * mid == n)
-	{
-		return (mid);
-	}
-	else if (mid * mid < n)
-	{
-		return (square_root(n, mid + 1, end));
-	}
-	else
-	{
-		return (square_root(n, start, mid - 1));
-	}
 }
 
 /**
-* _sqrt_recursion - computes natural square root of a number
-* @n: integer to be squared
+* _sqrt - computes natural square root of a number
+* @n: integer to be squaredi
+* @i: 
 * Return: n squared
 */
 
-int _sqrt_recursion(int n)
+int _sqrt(int n, int i)
 {
-	if (n < 0)
-	{
-		/* if n is negative, return -1 */
-		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-		/* if n is 0 or 1, return n */
-		return (n);
-	}
-	else
-	{
-		/* recursively calculate the square root of n */
-		return (square_root(n, 1, n));
-	}
+
 }
