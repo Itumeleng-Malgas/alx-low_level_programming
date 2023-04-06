7ae8a25b7fc3c90dd984ee71d667aa2d732e05eb
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-06 04:24:03 +0200

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,34 @@
 #include "main.h"
 
+/**
+* _sqrt_recursion - computes natural square root of a number
+* @n: base
+* Return: the root of n
+*/
+
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+		return (-1);
+	return (_sqrt(n, 0));
 }
 
 /**
 * _sqrt - computes natural square root of a number
-* @n: integer to be squaredi
-* @i: 
+* @n: integer to be squared
+* @i: power
 * Return: n squared
 */
 
 int _sqrt(int n, int i)
 {
+	/* not a natural root */
+	if (i * i > n)
+		return (-1);
 
+	/* found a the root */
+	if (i * i == n)
+		return (n);
+
+	return (_sqrt(n, i + 1));
 }
