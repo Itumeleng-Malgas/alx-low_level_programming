3ce4d99ff2c55b07e34c73119a316a4beb825cbb
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-06 06:25:45 +0200

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* _sqrt - finds the square root of a number
+* @n: non negative integer
+* Return: square of n
+*/
+
+int _sqrt(int n)
+{
+	int i = 1;
+
+	for (; i * i <= n; i++)
+	{
+	}
+	return (i - 1);
+}
+
+/**
+* is_prime_number - finds the prime number
+* @n: number > 2
+* Return: 1 success, 0 failure
+*/
+
+int is_prime_number(int n)
+{
+	static int div = 2;
+
+	if (n <= 0)
+		return (0);
+
+	if (div > _sqrt(n))
+		return (1);
+
+	if (n % div == 0)
+		return (0);
+
+	div++;
+	return (is_prime_number(n));
+}
