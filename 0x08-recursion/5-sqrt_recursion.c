a68103c60057335bce25573a6f9418b8b4e07734
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-06 06:57:11 +0200

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,19 +10,18 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_sqrt(n));
+	return (_sqrt(n, 1));
 }
 
 /**
 * _sqrt - computes natural square root of a number
 * @n: integer to be squared
+* @i: iterator
 * Return: n squared
 */
 
-int _sqrt(int n)
+int _sqrt(int n, int i)
 {
-	static int i = 1;
-
 	/* not a natural root */
 	if (i * i > n)
 		return (-1);
@@ -31,6 +30,5 @@ int _sqrt(int n)
 	if (i * i == n)
 		return (i);
 
-	i++;
-	return (_sqrt(n));
+	return (_sqrt(n, i++));
 }
