93fc7d4fcee984ca68730f9bad612ac6435b5e10
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-08 09:25:57 +0200

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of command line args
+ * @argv: an array of command line args
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+}
