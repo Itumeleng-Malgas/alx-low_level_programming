8b999b801eb361a168b584a0d5b56edd712670b5
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-08 09:55:27 +0200

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of command line args
+ * @argv: an array of command line args
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
