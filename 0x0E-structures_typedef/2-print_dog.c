9cb86f85e44f45fab54c7eb6319d5251e0283705
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-17 04:01:59 +0200

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints struct dog
+ * @d: pointer to dog struct
+ */
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
