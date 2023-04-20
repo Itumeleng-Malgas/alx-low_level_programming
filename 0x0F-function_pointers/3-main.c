148c7d955958f117076cd780929ec56184ebad49
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-20 07:10:26 +0200

    3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main -entry point
@@ -9,21 +10,28 @@
  *
  * Return: 0
  */
-int main(int argc, char *argv[] __attribute__ ((unused)))
+int main(int argc, char *argv[])
 {
+	char *op = argv[2];
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (!get_op_func(argv[2]) || argv[2][1] != '\0')
+	if (!get_op_func(op) || (
+				strcmp(op, "+") != 0 &&
+				strcmp(op, "*") != 0 &&
+				strcmp(op, "-") != 0 &&
+				strcmp(op, "/") != 0 &&
+				strcmp(op, "%") != 0))
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", get_op_func(op)(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
