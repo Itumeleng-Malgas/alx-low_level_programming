7d60ddeb108bebd1f3ee95c4f6839eba798721f8
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-21 08:04:15 +0200

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: separator, printed between numbers
+ * @n: number of args
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+
+	va
