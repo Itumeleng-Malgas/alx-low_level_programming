eae0deb66e5b6c1656e5bbc32d6dec05c5c7504b
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-28 20:01:18 +0200

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,6 @@
-#include "lists.h"
+#include <stdio.h>
+
+void __attribute__((constructor)) run_before_main(void);
 
 /**
  * run_before_main - prints to stdout before main function run
@@ -6,6 +8,6 @@
 
 void run_before_main(void)
 {
-	printf("You're beat! and yet,you must allow,\n"
-			"I bore my house upon my back!\n");
+	printf("You're beat! and yet,you must allow,"
+			"\nI bore my house upon my back!\n");
 }
