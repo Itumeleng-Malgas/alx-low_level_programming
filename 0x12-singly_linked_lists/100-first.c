d0f94538fa13fd787f7d6613f4ba3fe958659a88
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-28 19:05:20 +0200

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * run_before_main - prints to stdout before main function run
  */
 
-void __attribute__((constructor)) run_before_main()
+void __attribute__((constructor(101))) run_before_main()
 {
 	printf("You're beat! and yet,you must allow,\n"
 			"I bore my house upon my back!\n");
