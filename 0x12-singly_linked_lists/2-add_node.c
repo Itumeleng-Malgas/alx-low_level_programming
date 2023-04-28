3503d8f8a52b7099a275b20a7586f39739ec26a6
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-28 16:49:22 +0200

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,11 +14,11 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new == NULL)
 		return (NULL);
-	
-	new->str 	= strdup(str);
-	new->len 	= strlen(str);
+
+	new->str	= strdup(str);
+	new->len	= strlen(str);
 	new->next	= (*head);
-	(*head) 	= new;
+	(*head)		= new;
 
 	return (new);
 }
