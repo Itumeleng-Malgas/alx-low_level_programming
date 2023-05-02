c127d2dc6ab3c3f8c8f45f921d850fe029987d2b
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-05-02 23:02:53 +0200

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,15 +21,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (; i < idx && current != NULL; i++)
 		current = current->next;
 
-	/* existing node at idx */
-	if (i == idx)
+	/* index out of bounds */
+	if (i != idx)
 		return (NULL);
 
-	new_node		= malloc(sizeof(listint_t));
-	new_node->n		= n;
-	new_node->next	= current->next;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-	current->next	= new_node;
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	current = *head;
+	for (i = 0; i < idx - 1; i++)
+		current = current->next;
+
+	new_node->next = current->next;
+	current->next = new_node;
 
 	return (new_node);
 }
