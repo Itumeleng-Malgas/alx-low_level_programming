c7f7b5b91f37f0f0cd4c5d3b2d28e2840254ca77
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-05-05 23:00:21 +0200

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: decimal number
+ */
+
+void print_binary(unsigned long int n)
+{
+	/* create a mask and set MSB to 1 */
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int zero_flag = 0;
+
+	/* iterate over each bit of n */
+	while (mask != 0)
+	{
+		/* if bit in 'n' with mask match, print 1 and set zero flag */
+		if ((n & mask) != 0)
+		{
+			_putchar('1');
+			zero_flag = 1;
+		}
+
+		else if (zero_flag)
+			_putchar('0');
+
+		/* shift mask 1 bit to the right */
+		mask = mask >> 1;
+	}
+
+	/* if n = 0, print 0 */
+	if (!zero_flag)
+		_putchar('0');
+}
