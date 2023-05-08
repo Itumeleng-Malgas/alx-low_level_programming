b3bdd71bafd061d57a640eee77c24ad3ad05e966
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-05-09 00:25:17 +0200

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,4 +11,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+/************** cp ************/
+#define BUFFER_SIZE 1024
+void error_out(int code, const char* format, const char* arg);
+
 #endif /* MAIN_H */
